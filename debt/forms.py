67c85d8664372ebf873e0e51d0d9c5e66492f7f0
Visipuki <visipuki@mail.ru>
2014-11-08 06:30:53 -0800

    debt form is added

---
+++ debt/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth.models import User
+from spending.models import SpendingType
+
+
+class DebtForm(forms.Form):
+    name = forms.CharField(
+        max_length=32, required=True,
+        label='Имя планируемой траты*'
+    )
+    money = forms.IntegerField(
+        label ='Расход*',
+        required=True
+    )
+    owner = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        empty_label=None,
+        required=False,
+        label='Автор'
+    )
+    spendingType = forms.ModelChoiceField(
+        queryset=SpendingType.objects.all(),
+        required=True,
+        label='Тип траты*'
+    )
+    comment = forms.CharField(
+        max_length=32, required=False,
+        label='Комментарий'
+    )
+    PERIOD_CHOICES = (
+        ('m', 'ежемесячно'),
+    )
+    periodical = forms.ChoiceField(
+        empty_label='Разовый',
+        label='Периодичность',
+    )
