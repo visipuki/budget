733f3c8f9ade2275e72b9b7f454f1c0966ea54a7
Visipuki <visipuki@mail.ru>
2014-11-09 08:30:56 -0800

    is_active is added to account for management of accounts in future

--- account/models.py
+++ account/models.py
@@ -8,6 +8,7 @@ class Account(models.Model):
     is_cost_default = models.BooleanField()
     is_income_default = models.BooleanField()
     owner = models.ForeignKey(User)
+    is_active = models.BooleanField(default=True)
     class Meta:
         verbose_name = 'Кошелек'
         verbose_name_plural = 'Кошельки'
