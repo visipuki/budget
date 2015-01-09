5f8327c1c0e2cb838dc3853bc42df7c5f80102e9
alexey <alexey@dja.0.2.15>
2015-01-09 18:57:30 +0300

    filter is here

--- analytics/urls.py
+++ analytics/urls.py
@@ -2,7 +2,12 @@ from django.conf.urls import patterns, url
 from analytics import views
 
 
-urlpatterns = patterns('',
-                       url(r'^$', views.analyticsView),
-                       url(r'^(\d{2}-\d{2}-\d{4})_(\d{2}-\d{2}-\d{4})/$',
-                           views.analyticsView))
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.analyticsView),
+    url(r'^(\d{2}-\d{2}-\d{4})_(\d{2}-\d{2}-\d{4})/$', views.analyticsView),
+    url(
+        r'^'+'((owner_id|incomeType_id|date__gte|date__lte|spendingType_id)/(.+?)/)?'*5+r'$',
+        views.analyticsView
+    ),
+)
